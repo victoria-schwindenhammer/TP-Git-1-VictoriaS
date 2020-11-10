388ab8940a646200b22e6c177f7bc1cd8749ae09
Eliane <Eliane@MamieBlue.home>
2020-11-10 15:55:19 +0100

    mon premier commit

---
+++ src/main/java/com/mycompany/projetgit1/Client.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projetgit1;
+
+/**
+ *
+ * @author Victoria
+ */
+public class Client {
+    //ma nouvelle classe
+}
